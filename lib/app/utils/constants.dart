1d27b869aa69c167cf479f0436221e4af4b0036b
aashiq <ashik.cse.developer@gmail.com>
2022-01-22 02:55:02 +0600

    dashboard done

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -3,4 +3,4 @@ import 'dart:ui';
 const TAKA_SYMBOL = "৳";
 const EURO_SYMBOL = "€";
 const PER_PAGE_ITEM_LIMIT = 20;
-const APP_BASE_URL = "https://odootest.sindabad.com/";
+const APP_BASE_URL = "";
